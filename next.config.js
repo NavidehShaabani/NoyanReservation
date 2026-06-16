5541a267845449743657110e242de19accfc149e
Your navidehshaabani <navidehshaabani@gmail.com>
2026-06-16 21:03:32 +0330

    feat: اضافه کردن تنظیمات Netlify برای خروجی استاتیک

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const nextConfig = {
   reactStrictMode: true,
   turbopack: {},
+
+  output: "export", // ← این خط باعث میشه خروجی استاتیک بگیریم برای تنظیمات Netlify
+  images: {
+    unoptimized: true, // برای Netlify لازمه
+  },
 };
 
 module.exports = nextConfig;
